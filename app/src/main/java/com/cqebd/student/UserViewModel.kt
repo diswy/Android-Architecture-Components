4f4bc7bc7125cab020580d7ca7799b2c247ec3e3
xiaofu <githubdiswy@163.com>
2018-11-25 21:08:37 +0800

    init

---
+++ app/src/main/java/com/cqebd/student/UserViewModel.kt
@@ -0,0 +1,27 @@
+package com.cqebd.student
+
+import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class UserViewModel : ViewModel() {
+    private val user = MutableLiveData<User>()
+
+    init {
+        user.value = User("默认姓名",18)
+    }
+
+    fun getUser():LiveData<User>{
+        return user
+    }
+
+    fun modifyName(){
+        user.value = User("修改后的名字",12)
+
+    }
+
+    fun modifyName2(){
+        user.value = User("什么鬼",1111)
+    }
+}
